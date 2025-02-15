dbad3b16cb7e9bbafe725ba62a34ceb835024c95
Alexander <alexander5243188@yahoo.es>
2025-02-15 08:03:56 -0400

    datos personales
    Almacenar en una variable el nombre completo y luego mostrar esta información.

--- nivel_basico/operaciones_basicas/pedir_datos.js
+++ nivel_basico/operaciones_basicas/pedir_datos.js
@@ -1,4 +1,4 @@
 //- Crea una variable con tu nombre y muéstrala en la consola.
-let nombre_completo = "Alexander vega";
+let nombre_completo = "Alexander Richard Vega Fuentes";
 console.log("Hola, "+nombre_completo);
 
