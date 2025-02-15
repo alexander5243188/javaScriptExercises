2034a5095baae1be406e02f1e03e477e842bee89
Alexander <alexander5243188@yahoo.es>
2025-02-15 07:59:10 -0400

    Dados dos valores.
    Se pide dos valores y se realiza operaciones de suma, resta, división, multiplicación

---
+++ nivel_basico/operaciones_basicas/dos_variables.js
@@ -0,0 +1,13 @@
+// - Declara dos variables numéricas e imprime su suma, resta, multiplicación y división.
+let primer_valor = 2;
+let segundo_valor = 3;
+
+let suma = primer_valor + segundo_valor;
+let resta = primer_valor + segundo_valor;
+let multiplicacion = primer_valor * segundo_valor;
+let division = primer_valor / segundo_valor;
+
+console.log(primer_valor +" + "+ segundo_valor+" = "+suma);
+console.log(primer_valor + " - "+ segundo_valor+" = "+resta);
+console.log(primer_valor+" * "+segundo_valor+" = "+multiplicacion);
+console.log(primer_valor+" / "+segundo_valor+" = "+division);
---
+++ nivel_basico/operaciones_basicas/pedir_datos.js
@@ -0,0 +1,4 @@
+//- Crea una variable con tu nombre y muéstrala en la consola.
+let nombre_completo = "Alexander vega";
+console.log("Hola, "+nombre_completo);
+
