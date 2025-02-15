457752565c13cf15884fe49081f1ed0e7df222d5
Alexander <alexander5243188@yahoo.es>
2025-02-15 09:38:40 -0400

    grados celsius a fahrenheit
    Dados grados celsius convertir a grados fahrenheit

---
+++ nivel_basico/operaciones_basicas/convertir_grados.js
@@ -0,0 +1,8 @@
+// Escribe un programa que convierta grados Celsius a Fahrenheit.
+
+let grados_Celsius = 1;
+
+let grados_Fahrenheit = ( grados_Celsius * (9/5) ) + 32;
+
+console.log(grados_Celsius+ "°C = "+ grados_Fahrenheit+"°F 👀");
+
