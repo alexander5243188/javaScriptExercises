f8e808067e5676e45d91fc4e5770affb305aff43
Alexander <alexander5243188@yahoo.es>
2025-02-15 08:00:35 -0400

    Par impar
    Dado un numero entero, identificar si es par o impar.

---
+++ nivel_basico/operaciones_basicas/par_impar.js
@@ -0,0 +1,9 @@
+//- Escribe un programa que determine si un número es par o impar.
+let numero = 5;
+
+if ((numero/2) == true) {
+    console.log(numero+" -> Es par");        
+}else{
+    console.log(numero+" -> Es Impar");    
+}
+    
