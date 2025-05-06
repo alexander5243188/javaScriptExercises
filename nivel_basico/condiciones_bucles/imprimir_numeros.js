d883311cddf54fc76de290a6dbc171b173e48e85
Alexander <alexander5243188@yahoo.es>
2025-05-06 07:17:09 -0400

    imprimir numeros

---
+++ nivel_basico/condiciones_bucles/imprimir_numeros.js
@@ -0,0 +1,22 @@
+// Crea un bucle que imprima los números del 1 al 10.
+let limite = 10;
+let res_for = []; 
+let res_while = []; 
+let res_do_while = [];
+
+
+// For
+for (let index = 1; index <= limite; index++) {
+    res_for.push(index);     
+}
+console.log("Estructura for --> "+ res_for);
+
+// while
+wl = 1
+while (wl <= 10) {     
+    res_while.push(wl);
+    wl++;    
+}
+console.log("Estructura while --> "+ res_while);
+
+// Do while
